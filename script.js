c34cc26947913bd5df4988a1c01987e495cabc52
Bárbara Gleyce <bgleyceeeeeee@gmail.com>
2025-11-11 09:39:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
