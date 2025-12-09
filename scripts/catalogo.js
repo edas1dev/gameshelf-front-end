f2c6ce99cfab08e340ec185f361beaf936a820aa
edas1dev <erickdavi200308@gmail.com>
2025-12-09 17:27:14 -0300

    View details add in catalog

--- scripts/catalogo.js
+++ scripts/catalogo.js
@@ -6,7 +6,7 @@ const gamesListContainer = document.getElementById("games-list");
 async function fetchAndRenderGames() {
     gamesListContainer.innerHTML = '<p style="color:white; text-align:center;">Carregando catálogo...</p>';
     try {
-        const response = await fetch(`${API_URL}/games`);
+        const response = await fetch(`${API_URL}/games?ranking=popular&quantity=80`);
         if (!response.ok) {
             throw new Error("Falha ao carregar o catálogo de jogos.");
         }
@@ -33,7 +33,7 @@ function renderGames(games) {
         const card = document.createElement("div");
         card.classList.add("game-card");
         const imageUrl = game.backgroundImage || '';
-        const rating = game.gameData?.rating ? `${game.gameData.rating} / 5` : 'N/A';
+        const rating = game.gameData?.metacritic ? `${game.gameData.metacritic} / 5` : 'N/A';
         
         card.innerHTML = `
             <div class="game-image-container">
@@ -42,7 +42,7 @@ function renderGames(games) {
             <div class="game-info">
                 <h3>${game.title}</h3>
                 <p class="game-rating">⭐ Nota: <strong>${rating}</strong></p>
-                <a href="detalhes.html?gameId=${game.id}" class="btn-primary-small">Ver Detalhes</a>
+                <a href="game-detail.html?gameId=${game.id}" class="btn-primary-small">Ver Detalhes</a>
             </div>
         `;
         gamesListContainer.appendChild(card);
---
+++ scripts/game-detail.js
@@ -0,0 +1,83 @@
+import { API_URL, logout, getToken } from './utils.js';
+
+const gameId = new URLSearchParams(window.location.search).get("gameId");
+
+const headerImg = document.getElementById("header-image");
+const boxImg = document.getElementById("box-image");
+const gameTitle = document.getElementById("game-title");
+const gameRating = document.getElementById("game-rating");
+const gameDescription = document.getElementById("game-description");
+const screenshotsContainer = document.getElementById("screenshots");
+const reviewsContainer = document.getElementById("reviews-container");
+const btnAvaliar = document.getElementById("btn-avaliar");
+
+async function loadGameDetails() {
+    try {
+        const token = getToken()
+
+        if(!token){
+            alert('Vá se lascar')
+            return
+        }
+
+        const response = await fetch(`${API_URL}/games/details/${gameId}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+        });
+        const game = await response.json();
+        console.log(game.title)
+
+        headerImg.src = game.backgroundImage;
+        boxImg.src = game.backgroundImageAdditional;
+        gameTitle.textContent = game.title;
+        gameRating.textContent = game.rating
+            ? `⭐ ${game.rating} / ${game.ratingTop}`
+            : "Sem Avaliação";
+        gameDescription.innerHTML = game.description || "Sem descrição.";
+        screenshotsContainer.innerHTML = "";
+        game.screenshots?.slice(0, 6).forEach(img => {
+            const el = document.createElement("img");
+            el.src = img;
+            screenshotsContainer.appendChild(el);
+        });
+
+        loadReviews();
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+// Buscar avaliações
+async function loadReviews() {
+    const response = await fetch(`${API_URL}/reviews?gameId=${gameId}`, {
+        headers: { "Authorization": `Bearer ${getToken()}` }
+    });
+
+    const reviews = await response.json();
+
+    if (!reviews.length) {
+        reviewsContainer.innerHTML = "<p>Nenhuma avaliação encontrada.</p>";
+        return;
+    }
+
+    reviewsContainer.innerHTML = "";
+    reviews.forEach(r => {
+        const card = document.createElement("div");
+        card.classList.add("review-card");
+        card.innerHTML = `
+            <strong>Nota: ${r.rating}/10</strong>
+            <p>${r.content}</p>
+        `;
+        reviewsContainer.appendChild(card);
+    });
+}
+
+btnAvaliar.addEventListener("click", () => {
+    window.location.href = `avaliacao.html?gameId=${gameId}`;
+});
+
+loadGameDetails();
