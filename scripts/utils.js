bf4ddca825c83a36319723fc6e8fa87d7ba092c0
edas1dev <erickdavi200308@gmail.com>
2026-01-05 09:38:50 -0300

    update for utils.js

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://20.201.77.123:3000";
+const API_URL = "https://gameshelf-back-end.onrender.com";
 
 export function getToken() {
   return localStorage.getItem("access_token");
